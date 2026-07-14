500df63ad0a60f309cc0cee044d49b50b4673fe6
DALOTE888 <anda80041@gmail.com>
2026-07-14 23:11:47 +0700

    AndaNarak

--- Anda_Narak.py
+++ Anda_Narak.py
@@ -1,15 +1,40 @@
 print("คำนวณกำไรค้าอาวุธเถื่อน")
 #จำนวณ
 quantity = int(input("จำนวนปืนที่รับมาขาย : "))
-#
-cost_price = int(input("ต้นทุนของปืนที่รับมาต่อละกระบอก : =" *{quantity}))
-print("จำนวณปืนทั้งหมด ={cost_price}")
-#จำนวณ
-sell_price= int(input("ราคาที่จะนำไปขายต่อ : "))
-#จำนวณ
-team_members= int(input("จำนวนลูกน้องในทีมที่ไปทำงาน : "))
+cost_price = int(input("ต้นทุนของปืนที่รับมาต่อละกระบอก : "))
 
+#ราคาต้นทุนของปืนทั้งหมด
+total_cost = cost_price * quantity
+print("กำไร" , total_cost)
+
+#จะขายปืนเท่าไหร่
+sell_price= int(input("ราคาที่จะนำไปขายต่อ : "))
+Gumrai = sell_price*quantity
+print("ถ้าขายหมดในราคานี้จะได้เงินทั้งหมด" , Gumrai )
+
+#Boss_Anda = 80%
+Boss_Anda = Gumrai *80 /100 
+print("บอสอันดาถือหุ้น80% แปลวาบอยจะได้เงินไป : " , Boss_Anda)
+
+#เหลือ
+Left = Gumrai - Boss_Anda 
+print("จะเหลือเงิน", Left)
+#team_members = 1% ต่อคน
+#ขี้ค่า
+team_members = int(input("จำนวนลูกน้องในทีมที่ไปทำงาน :"))
+team_money = Left *1 /100
+moneyReally = team_members*team_money
+if moneyReally > Left:
+    print("ไม่ต้องเอา")
+else:
+    print("ลูกน้องจะได้เงิน" ,moneyReally)
+
+
+print("สรุป")
 print("------------------")
 
-print(f"ต้นทุนทั้งหมด{cost_price} \n รายรับทั้งหมด{sell_price}")
-
+print("เงินกำไรของเราเหลือทั้งหมด กำไรสุทธิ :" , Left - moneyReally)
+print("ต้นทุน :" , total_cost)
+print("รายรับทั้งหมด: " ,Gumrai)
+print("จำนวนที่ให้บอสอันดา", Boss_Anda)
+print("จำนวนเงินที่ลูกน้องได้ต่อคน", team_money)
--- Andastrong.py
+++ Andastrong.py
@@ -1,5 +1,32 @@
-name = str(input("จำนวนปืนที่รับมาขาย : "))
-age = int(input("จำนวนปืนที่รับมาขาย : "))
-tall = int(input("จำนวนปืนที่รับมาขาย : "))
-Strong = int(input("จำนวนปืนที่รับมาขย : "))
+name = str(input("ขื่ออะไร : "))
+age = int(input("อายุ : "))
+tall = int(input("สูงเท่าไหร่ : "))
+Strong = int(input("ความแข็งแกร่ง : "))
 money = int(input("เงินติดตัว :"))
+Stat = tall+Strong+money
+
+if tall >= 170:
+    print("ผ่านที่170 คุณคือ" , tall ,"คุณผ่าน")
+else:
+    print("ไม่ผ่านไปไกลๆ")
+
+if Strong > 100:
+    print("ผ่านที่100 คุณได้ :" , Strong ,"คุณผ่าน")
+else:
+    print("ไม่ผ่าน")
+
+if money > 30:
+    print("ผ่านที่20 :" , money , "คุณผ่าน")
+else:
+    print("ไปไกลๆไอจน")
+
+if Stat > 300:
+    print("ขี้ค่า")
+elif Stat > 350:
+    print("ลูกน้องนิดนึง")
+elif Stat > 400:
+    print("บอดี้การ์ด")
+elif Stat > 450:
+    print("มือขวา")
+else:
+    print("ไปตายไป")
