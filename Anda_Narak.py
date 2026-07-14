8e7f824695bc0ce206267edb5f6bf38a6dcac037
Student <Student@Train11-5-PC121.control.ad.sit.kmutt.ac.th>
2026-07-14 16:17:40 +0700

    \Khonloh

---
+++ Anda_Narak.py
@@ -0,0 +1,15 @@
+print("คำนวณกำไรค้าอาวุธเถื่อน")
+#จำนวณ
+quantity = int(input("จำนวนปืนที่รับมาขาย : "))
+#
+cost_price = int(input("ต้นทุนของปืนที่รับมาต่อละกระบอก : =" *{quantity}))
+print("จำนวณปืนทั้งหมด ={cost_price}")
+#จำนวณ
+sell_price= int(input("ราคาที่จะนำไปขายต่อ : "))
+#จำนวณ
+team_members= int(input("จำนวนลูกน้องในทีมที่ไปทำงาน : "))
+
+print("------------------")
+
+print(f"ต้นทุนทั้งหมด{cost_price} \n รายรับทั้งหมด{sell_price}")
+
